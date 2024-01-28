b932dcc12cf6919ae104e355c1f35d9ae9e7fa58
Bruno de Santana <contatobrunosantana@icloud.com>
2024-01-28 05:14:52 -0300

    Initial commit

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+  @Input() weatherDatasInput!: WeatherDatas;
+  //Dados da previsao do tempo recebidos pelo componente pai weather-home
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
+
+
+}
